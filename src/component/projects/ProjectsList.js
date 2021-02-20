78640b587acaabf74a6c3b0268208995fc7b2a70
saujanya01 <saujanyatiwari455@gmail.com>
2021-02-20 14:30:24 +0530

    yolo

--- src/component/projects/ProjectsList.js
+++ src/component/projects/ProjectsList.js
@@ -9,7 +9,7 @@ function ProjectsList(props) {
             <p>{props.data.techStack}</p>
             <p>{props.data.desc}</p>
             <div className="projects-link">
-                {props.data.demo && <p><a href={props.data.demo} target="__blank">Live Demo</a></p>}
+                {props.data.demo && <p><a href={props.data.demo} target="__blank">Demo</a></p>}
                 {props.data.github && <a href={props.data.github} target="__blank"><img src="./image/gitlogo.svg"></img></a>}
             </div>
         </div>
