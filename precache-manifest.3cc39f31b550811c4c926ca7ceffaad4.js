6c67f5509261e3124a0460eb3a6d14c116cd9661
Saujan ya Tiwari <saujanyatiwari455@gmail.com>
2023-02-19 12:44:10 +0530

    Updates

---
+++ precache-manifest.3cc39f31b550811c4c926ca7ceffaad4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2451fb275d97350e8b5e",
+    "url": "/static/css/main.5849e397.chunk.css"
+  },
+  {
+    "revision": "2451fb275d97350e8b5e",
+    "url": "/static/js/main.2451fb27.chunk.js"
+  },
+  {
+    "revision": "a223c62900db244708b0",
+    "url": "/static/js/1.a223c629.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b0532cf6add90a6a8161f3d1989d5e42",
+    "url": "/index.html"
+  }
+];
