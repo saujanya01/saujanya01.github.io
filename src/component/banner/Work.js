f57154456a0bf426a79d12feab7493f68a94bee6
Saujan ya Tiwari <saujanyatiwari455@gmail.com>
2023-08-21 12:34:48 +0530

    work and contact updates

--- src/component/banner/Work.js
+++ src/component/banner/Work.js
@@ -4,31 +4,45 @@ import WorkItems from "./Items";
 function Work() {
 
     const work = [
-        {
-            name: "SDE @ HyperVue AI",
-            link: "https://hypervue.ai",
-            date: "February 2022 - Present",
-            desc: "Working as a fullstack engineer."
-        },
-        {
-            name: "Analyst @ Deloitte USI",
-            link: "https://www2.deloitte.com/ui/en.html",
-            date: "September 2021 - February 2022",
-            desc: "Worked in testing of mainframe applications."
-        },
-        {
-            name: "Summer Analyst @ Axis Bank (BIU)",
-            link: "https://www.axisbank.com/",
-            date: "May 2020 - June 2020",
-            desc: "Interned with Outbound Call Centre Team on a Call Centre Analytics project to identify the best practices that are being followed in call Centres across the globe."
-        },
-        {
-            name: "Mentor/TA @ CodeAsylums",
-            link: "https://codeasylums.com/",
-            date: "Dec 2019 - October 2020",
-            desc: "Taught Python, Machine Learning and Digital Image Processing to bootcamp students."
-        }
-    ]
+      {
+        name: "Full-Stack Engineer @ CTRL Robotics",
+        link: "https://ctrlrobotics.com/",
+        date: "August 2023 - Present",
+        desc: "Working as a fullstack engineer.",
+      },
+      {
+        name: "Full-Stack Engineer @ Youshd",
+        link: "https://youshd.com/",
+        date: "May 2023 - July 2023",
+        desc: "Worked as a fullstack engineer.",
+      },
+      {
+        name: "Full-Stack Engineer @ HyperVue AI",
+        link: "https://hypervue.ai",
+        date: "February 2022 - April 2023",
+        desc: "Worked as a fullstack engineer.",
+      },
+      {
+        name: "Analyst @ Deloitte USI",
+        link: "https://www2.deloitte.com/ui/en.html",
+        date: "September 2021 - February 2022",
+        desc: "Worked in testing of mainframe applications.",
+      },
+      {
+        name: "Summer Analyst @ Axis Bank (BIU)",
+        link: "https://www.axisbank.com/",
+        date: "May 2020 - June 2020",
+        desc:
+          "Interned with Outbound Call Centre Team on a Call Centre Analytics project to identify the best practices that are being followed in call Centres across the globe.",
+      },
+      {
+        name: "Mentor/TA @ CodeAsylums",
+        link: "https://codeasylums.com/",
+        date: "Dec 2019 - October 2020",
+        desc:
+          "Taught Python, Machine Learning and Digital Image Processing to bootcamp students.",
+      },
+    ];
 
     return(
         <div className="banner-details-card">
--- src/component/contact/Contact.js
+++ src/component/contact/Contact.js
@@ -3,17 +3,24 @@ import React from "react";
 import "../css/contact.css";
 
 function Contact() {
-    return(
-        <div className="contact">
-            <p>Contact</p>
-            <hr/>
-            <div className="contact-icon">
-                <a href="mailto:saujanyatiwari455@gmail.com" target="__blank">Email</a>
-                <a href="https://github.com/saujanya01" target="__blank">GitHub</a>
-                <a href="https://www.linkedin.com/in/saujanya01/" target="__blank">Linkedin</a>
-            </div>
+    return (
+      <div className="contact">
+        <p>Contact</p>
+        <hr />
+        <div className="contact-icon">
+          <a href="mailto:saujanyatiwari455@gmail.com" target="__blank">
+            Email
+          </a>
+          <a href="https://github.com/saujanya01" target="__blank">
+            GitHub
+          </a>
+          <a href="https://www.linkedin.com/in/saujanya01/" target="__blank">
+            Linkedin
+          </a>
+          <a href="https://www.youtube.com/channel/UCkSdUWv_dJECLvqBmTjuVTw" target="__blank">Youtube</a>
         </div>
-    )
+      </div>
+    );
 }
 
 export default Contact;
